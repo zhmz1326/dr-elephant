860dbe6ad2830b8ffeb88fc97966fb33b010b5fd
Chinmay Sumant <chinmayms@users.noreply.github.com>
2018-06-14 09:55:08 +0530

    Rerevert "Dr. Elephant Tez Support working patch (#313)" including attribution.
    This reverts commit e3fd598d048838006f2da26fe64538bd39d746ed.
    
    Co-authored-by: Abhishek Das <abhishekdas99@users.noreply.github.com>

--- test/com/linkedin/drelephant/tez/fetchers/TezFetcherTest.java
+++ test/com/linkedin/drelephant/tez/fetchers/TezFetcherTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 LinkedIn Corp.
+ * Copyright 2017 Electronic Arts Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -12,19 +12,16 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
+ *
  */
-
-package com.linkedin.drelephant.mapreduce.fetchers;
+package com.linkedin.drelephant.tez.fetchers;
 
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.linkedin.drelephant.util.ThreadContextMR2;
 import org.junit.Assert;
 import org.junit.Test;
+import com.linkedin.drelephant.util.ThreadContextMR2;
 
-
-public class MapReduceFetcherHadoop2Test {
+public class TezFetcherTest {
 
   @Test
   public void testDiagnosticMatcher() {
